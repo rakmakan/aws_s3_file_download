c8fb8d56c8378fe2cc918e02b17e7dfa255584f5
rakmakan <46840553+rakmakan@users.noreply.github.com>
2021-01-15 13:51:23 +0530

    Updated boto.py

--- boto_test.py
+++ boto_test.py
@@ -14,16 +14,16 @@ else:
     os.mkdir(download_folder)
 
 
-AWS_ACCESS_KEY_ID = 'AKIAXRUJHASABRVOZPHG'
-AWS_SECRET_ACCESS_KEY = '6KBVdaruB7yx+0wUvNCv0WNx9SsjLCMCJfsazla0'
-Bucketname = 'aktana-novartis-cdf' 
+AWS_ACCESS_KEY_ID = # Your access key ID
+AWS_SECRET_ACCESS_KEY = # secret key
+Bucketname = # Name of your bucket 
 
 session = boto3.Session(aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-client = session.client(service_name = 's3',endpoint_url='https://aktana-novartis-cdf.s3.amazonaws.com')
+client = session.client(service_name = 's3',endpoint_url= '') #enter your endpoint
 
 print(client.list_objects(Bucket = Bucketname) )
 # exit()  
-response = client.list_objects_v2(Bucket='aktana-novartis-cdf')
+response = client.list_objects_v2(Bucket=Bucketname)
 
 for content in response['Contents']:
     obj_dict = client.get_object(Bucket=Bucketname, Key=content['Key'])
